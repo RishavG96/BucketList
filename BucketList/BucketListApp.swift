aefa0ce1ed685c72dfffd4cd98b80c8b6542fa3c
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-25 06:45:25 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Rishav Gupta on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
