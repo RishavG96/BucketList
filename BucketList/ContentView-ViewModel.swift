179d507eb83499eeb816e09f3443cd0d89fcada7
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-25 10:38:05 +0530

    Bucket List complete

--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -6,9 +6,13 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
+    // View Model = have properties and methods here - easier for tests
+    // Manipulation of views is a great candicate for ViewModel
+    
     @MainActor class ViewModel: ObservableObject {
         // @MainActor is responsible for running all user interface updates and adding that attribute to our class means whatever we run in this class to run on the MainActor - responsible for making UI updates
         // As before, when using ObservableObject, when we made objects, ObjservedObjects or StateObject, Swift silently inferred the MainActor without us asking for it.
@@ -19,8 +23,69 @@ extension ContentView {
         
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         
-        @Published var locations = [Location]()
-        
+        @Published private(set) var locations: [Location] // read all you want, but you can't set from outside
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+        
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaced")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data")
+            }
+        }
+        
+        func addLocation() {
+            let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success {
+//                        Task { // this makes the task run in background thread
+//                            await MainActor.run { // this will make it run in main thread
+//                                self.isUnlocked = true
+//                            }
+//                        }
+                        Task { @MainActor in // this will make the task run in main thread directly
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // problem
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+        }
     }
 }
--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -12,61 +12,67 @@ struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
     
     var body: some View {
-        ZStack {
-            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
-//                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
-                MapAnnotation(coordinate: location.coordinate) {
-                    VStack {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundColor(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(Circle())
-                        
-                        Text(location.name)
-                            .fixedSize()
-                    }
-                    .onTapGesture {
-                        viewModel.selectedPlace = location
+        if viewModel.isUnlocked {
+            ZStack {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    //                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                            
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
                     }
                 }
-            }
                 .ignoresSafeArea()
-            
-            Circle()
-                .fill(.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack {
-                Spacer()
                 
-                HStack {
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                
+                VStack {
                     Spacer()
                     
-                    Button {
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: viewModel.mapRegion.center.latitude, longitude: viewModel.mapRegion.center.longitude)
-                        viewModel.locations.append(newLocation)
-                    } label: {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
             }
-        }
-        .sheet(item: $viewModel.selectedPlace) { place in // takes an optional bind to work with (Location?) and also receives the unwrapped place to work with (place)
-            EditView(location: place) { newLocation in
-                if let index = viewModel.locations.firstIndex(of: place) {
-                    viewModel.locations[index] = newLocation
-                    print(viewModel.locations)
+            .sheet(item: $viewModel.selectedPlace) { place in // takes an optional bind to work with (Location?) and also receives the unwrapped place to work with (place)
+                EditView(location: place) { newLocation in
+                    viewModel.update(location: newLocation)
                 }
             }
+        } else {
+            Button("Unlock Places") {
+                viewModel.authenticate()
+            }
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
         }
     }
 }
